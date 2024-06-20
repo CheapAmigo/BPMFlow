0adaddf8c4dff17227a9c6c1c1ee086fdbd5626e
Cheaposhila <slava.volkov.228@yandex.ru>
2024-06-20 23:17:46 +0300

    Добавлен фронтэтнд\Bootstrap для страницы авторизации и регистрации. На странице регистрации добавлена логика уведомления о проблемах в вводимых данных.

--- PMSapp/forms.py
+++ PMSapp/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser, Department, Task
@@ -10,7 +11,7 @@ class CustomUserCreationForm(UserCreationForm):
     passport_data = forms.CharField(max_length=100, required=True, help_text='Обязательное поле.')
     department = forms.ModelChoiceField(queryset=Department.objects.all(), required=True, help_text='Обязательное поле.')
     position = forms.CharField(max_length=100, required=False, label='Должность', widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}))
-    responsibilities = forms.CharField(required=False, label='Должностные обязанности', help_text='Разделяйте обязанности запятой', widget=forms.Textarea(attrs={'class': 'form-control', 'readonly': 'readonly'}))
+    responsibilities = forms.CharField(required=False, label='Должностные обязанности', help_text='Пишите аккуратно', widget=forms.Textarea(attrs={'class': 'form-control', 'readonly': 'readonly'}))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,6 +21,21 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'patronymic', 'phone_number', 'passport_data', 'department', 'role', 'position', 'responsibilities', 'password1', 'password2')
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not re.match(r'^[\w.@+-]+$', username):
+            raise forms.ValidationError('Имя пользователя может содержать только буквы, цифры и @/./+/-/_. Пробелы недопустимы.')
+        return username
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Пароли не совпадают.')
+        if password2 and (len(password2) < 8 or password2.isdigit() or password2 == password2.lower()):
+            raise forms.ValidationError('Ваш пароль должен содержать не менее 8 символов, не быть полностью цифровым, строчным или похожим на другие личные данные и не быть регулярным паролем.')
+        return password2
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(max_length=254, required=True, help_text='Обязательное поле.')
